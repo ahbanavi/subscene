73f29837bbda85d9fa549e20b5af73f266d65668
Amirhossein Banavi <ahbanavi@gmail.com>
2024-05-06 22:58:35 +0330

    multi bot

--- src/tg_upload.py
+++ src/tg_upload.py
@@ -10,6 +10,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 SUBS_PATH = os.getenv("SUBS_PATH", "./Subscene Files DB")
 
+BOT_TOKENS = BOT_TOKEN.split(",")
+BOTS_LEN = len(BOT_TOKENS)
+MAX_UPLOAD_TRYS = BOTS_LEN
+
 logging.basicConfig(
     level=logging.WARNING,
     format="%(asctime)s | %(levelname)s | %(message)s",
@@ -38,6 +42,7 @@ cur = conn.cursor(dictionary=True)
 
 
 # get all subs that lang = farsi_persian
+logging.info("Getting all farsi_persian subs")
 cur.execute(
     "SELECT * FROM all_subs WHERE lang = 'farsi_persian' AND tg_post_id IS NULL ORDER BY fileLink,title"
 )
@@ -47,67 +52,97 @@ def strip_html(text):
     return text.replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp;")
 
 
-def upload():
-    for sub in cur.fetchall():
-        with open(f"{SUBS_PATH}/{sub['fileLink']}", "rb") as f:
-
-            caption_list = []
-            if sub["title"]:
-                caption_list.append(f"📽 <b>{strip_html(sub['title'])}</b>")
-
-            if sub["author_name"]:
-                caption_list.append(f"✍️ <i>{strip_html(sub['author_name'])}</i>")
-
-            if sub["imdb"]:
-                caption_list.append(
-                    f"🎥 <a href='https://www.imdb.com/title/tt{sub['imdb']}'>IMDb</a> | <code>tt{sub['imdb']}</code>"
-                )
-
-            caption_list.append(f"🆔 <code>{sub['id']}</code>")
-
-            if sub["date"]:
-                caption_list.append(f"📅 {sub['date']}")
-
-            if sub["comment"]:
-                caption_list.append(f"💬 {strip_html(sub['comment'])}")
-
-            release = strip_html(sub["releases"])
-
-            release = release.translate({ord(i): None for i in '[",]'}).strip("-_ ")
-            if release:
-                caption_list.append(f"<pre>{release}</pre>")
-
-            caption = "\n".join(caption_list)
-
-            if len(caption) > 1000:
-                caption = caption[:1000]
-                if release:
-                    if caption.count("<pre>") == 1 and caption.count("</pre>") == 0:
-                        caption += "</pre>"
-
-            files = {"document": f}
-            params = {
-                "chat_id": CHANNEL_ID,
-                "caption": caption,
-                "parse_mode": "HTML",
-            }
-            response = requests.post(
-                f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument",
-                files=files,
-                params=params,
+def upload(sub, file_path, caption, trys=0, bot_id=0):
+    logging.info(f"Uploading {sub['title']}, id: {sub['id']} with bot {bot_id}")
+    file = open(file_path, "rb")
+    files = {"document": file}
+    params = {
+        "chat_id": CHANNEL_ID,
+        "caption": caption,
+        "parse_mode": "HTML",
+    }
+    try:
+        response = requests.post(
+            f"https://api.telegram.org/bot{BOT_TOKENS[bot_id]}/sendDocument",
+            files=files,
+            params=params,
+        )
+        file.close()
+    except:
+        logging.error(f"Error sending request to bot {bot_id}")
+        time.sleep(1)
+        return upload(sub, file_path, caption, trys + 1, (bot_id + 1) % BOTS_LEN)
+    if response.status_code == 200:
+        cur.execute(
+            "UPDATE all_subs SET tg_post_id = ? WHERE id = ? LIMIT 1",
+            (response.json()["result"]["message_id"], sub["id"]),
+        )
+        return bot_id
+    elif response.status_code == 429:
+        if trys < MAX_UPLOAD_TRYS:
+            logging.info(
+                f"Rate limit exceeded with bot {bot_id}, retrying with next bot..."
             )
-            if response.status_code == 200:
-                cur.execute(
-                    "UPDATE all_subs SET tg_post_id = ? WHERE id = ? LIMIT 1",
-                    (response.json()["result"]["message_id"], sub["id"]),
-                )
-                time.sleep(3)
-            elif response.status_code == 429:
-                time.sleep(response.json()["parameters"]["retry_after"])
-            else:
-                logging.error(
-                    f"Error uploading {sub['title']}, id: {sub['id']}: {response.text}"
-                )
+            return upload(sub, file_path, caption, trys + 1, (bot_id + 1) % BOTS_LEN)
+        else:
+            logging.warning(
+                f"Rate limit exceeded with all bots, waiting for {response.json()['parameters']['retry_after']} seconds..."
+            )
+            time.sleep(response.json()["parameters"]["retry_after"])
+            return upload(sub, file_path, caption, 0, bot_id)
+    else:
+        logging.error(
+            f"Error uploading {sub['title']}, id: {sub['id']}: {response.text}"
+        )
+        return bot_id
 
 
-upload()
+def main():
+    bot_id = 0
+    for sub in cur.fetchall():
+        file_path = f"{SUBS_PATH}/{sub['fileLink']}"
+        if not os.path.exists(file_path):
+            logging.warning(f"File {file_path} not found, skipping...")
+            continue
+
+        caption_list = []
+        if sub["title"]:
+            caption_list.append(f"📽 <b>{strip_html(sub['title'])}</b>")
+
+        if sub["author_name"]:
+            caption_list.append(f"✍️ <i>{strip_html(sub['author_name'])}</i>")
+
+        if sub["imdb"]:
+            caption_list.append(
+                f"🎥 <a href='https://www.imdb.com/title/tt{sub['imdb']}'>IMDb</a> | <code>tt{sub['imdb']}</code>"
+            )
+
+        caption_list.append(f"🆔 <code>{sub['id']}</code>")
+
+        if sub["date"]:
+            caption_list.append(f"📅 {sub['date']}")
+
+        if sub["comment"]:
+            caption_list.append(f"💬 {strip_html(sub['comment'])}")
+
+        release = strip_html(sub["releases"])
+
+        release = release.translate({ord(i): None for i in '[",]'}).strip("-_ ")
+        if release:
+            caption_list.append(f"<pre>{release}</pre>")
+
+        caption = "\n".join(caption_list)
+
+        if len(caption) > 1000:
+            caption = caption[:1000]
+            if release:
+                if caption.count("<pre>") == 1 and caption.count("</pre>") == 0:
+                    caption += "</pre>"
+
+        bot_id = upload(sub, file_path, caption, 0, bot_id)
+
+
+if __name__ == "__main__":
+    main()
+    cur.close()
+    conn.close()
